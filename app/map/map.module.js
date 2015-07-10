41936f377f5fb6f4f419db1203f90e5e19b0945b
tiffanyRossyRobinson <trossys@gmail.com>
2015-07-10 18:36:29 -0400

    made the map pop up! its super basic though

--- app/map/map.module.js
+++ app/map/map.module.js
@@ -15,12 +15,4 @@
 
     })
 
-  .config(function(uiGmapGoogleMapApiProvider) {
-      uiGmapGoogleMapApiProvider.configure({
-          key: 'AIzaSyBuurnC0j7csdtlgA4I0IDJu2AA4kLPwnc',
-          v: '3.17',
-          libraries: 'weather,geometry,visualization'
-      });
-  })
-
 }());
