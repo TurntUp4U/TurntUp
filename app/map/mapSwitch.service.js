e76594738407ef4a25362ae04590939d664b591f
tiffanyRossyRobinson <trossys@gmail.com>
2015-07-11 17:41:52 -0400

    adds turnt up location and deletes previous turn up locations from you

--- app/map/mapSwitch.service.js
+++ app/map/mapSwitch.service.js
@@ -14,6 +14,17 @@
     var urlMap = 'http://tiy-fee-rest.herokuapp.com/collections/turntUpMap';
 
     var addToMap = function (whereYouAre) {
+      // console.log("this person is turnt up", whereYouAre.displayName);
+      getFromMap().then(function(spots){
+        _.forEach(spots.data, function(oneSpot){
+          if(oneSpot.displayName === whereYouAre.displayName){
+            if(oneSpot._id !== '559ffdb7e9e0393733000001'){ //wont let me delete this id. we can use a different server.
+              removeFromMap(oneSpot._id);
+            }
+          }
+        });
+      });
+
       $http.post(urlMap, whereYouAre).success(function (response) {
         console.log(response);
       }).error(function (err) {
@@ -25,11 +36,20 @@
        return $http.get(urlMap);
     };
 
+    var removeFromMap = function(id){
+          return $http.delete(urlMap + '/' + id).success(function (response) {
+            console.log(response);
+          }).error(function (err) {
+            console.log(err);
+          });
+        };
+
 
     return {
       switchCoords: switchCoords,
       addToMap: addToMap,
-      getFromMap: getFromMap
+      getFromMap: getFromMap,
+      removeFromMap: removeFromMap
     }
   });
 }());
